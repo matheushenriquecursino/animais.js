bff6341f1ebdfd9436fbd4fe163f9178fd898602
matheushenriquecursino <matheus.cursino@escola.pr.gov.br>
2024-08-12 08:30:09 -0300

    Create animais.js

---
+++ animais.js
@@ -0,0 +1,33 @@
+DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<script src="animais.js";></script>
+</body>
+
+function imprimiNome()
+{
+console.log(animal.nome)
+console.log(animal.nome)
+console.log(animal.nome)
+} catch (error) {
+console.log("não existe variavel com o nome que +"
+"o nome que voce tentou imprimir na função impriminome")
+}
+
+console.log("o codigo continua executando");
+
+
+}
+
+animal = new Object();
+animal.nome = "cachorro,toto";
+animal.nome = "gato,tom";
+animal.nome = "peixe,peixonauta";
+
+imprimiNome();
